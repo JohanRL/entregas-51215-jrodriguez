c74315bced92464543a773d8f7f1901ee5085d42
Johan Rodriguez <johanr304@gmail.com>
2023-04-11 21:08:34 -0500

    entrega#3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.urlencoded({extended:true}))
 
-// Ruta a filtrado de productos por ID con un limite, si no hay limite entonces da el array completo
+
 app.get('/products', (req, res) => {
     const limit = parseInt(req.query.limit)
     let products = productManager.getProducts()
@@ -18,35 +18,22 @@ app.get('/products', (req, res) => {
     }
 })
 
-
-// Ruta a filtrado de productos por querys
 app.get('/products/:productId', (req, res) => {
     const products = productManager.getProducts()
     const productsId = parseInt(req.params.productId)
     const product = products.find(product => product.id === productsId)
     if (!product) {
-        const error = {error: 'Producto no encontrado'}
+        const error = {error: 'Product not found'}
         return res.status(404).send(error)
     }
     res.send(product)
 })
 
 
-// Iniciar el servidor en puerto 8080
 app.listen(8080, () => {
-    console.log('Server corriendo en puerto 8080')
+    console.log('Server running on 8080')
 })
 
-// Zona Testing
-
-// Recibir en consola el array vacio
-console.log(productManager.getProducts())
-
-//Sumar nuevo producto al array
-productManager.addProduct("manzana", "este es un producto prueba", 200, "sin imagen", "fruta1", 25) 
-productManager.addProduct("banana", "este es un segundo producto prueba", 200, "sin imagen", "fruta2", 25) 
-productManager.addProduct("pera", "este es un tercer producto prueba", 200, "sin imagen", "fruta3", 25) 
-
-
-// Recibir en consola el array vacio
-console.log(productManager.getProducts())
+productManager.addProduct("apple", "red", 100, "imagen.jpg", "tgyujh", 20) 
+productManager.addProduct("pasta", "spaghetti", 100, "imagen.jpg", "fghfh", 22) 
+productManager.addProduct("rice", "normal", 50, "imagen.jpg", "fghfgh", 23) 
